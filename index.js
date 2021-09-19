e28d4fd509fea3fb2aca3d2819d4c23845f3afcb
juln <66806955+z-juln@users.noreply.github.com>
2021-09-19 22:50:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = new (require('koa'))()
 app.use(async ctx => {
-  ctx.body = "<h1>hello world. I'm juln.</h1>"
+  ctx.body = "<h1>hello world! I'm juln.</h1>"
 })
 app.listen(7001, () => {
   console.log('http://127.0.0.1:7001')
